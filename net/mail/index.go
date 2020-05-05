262e3556606b3a5b77e82fb403562decbbdea4d0
victor <victor@xurw.com>
2020-05-05 20:48:56 +0800

    up

--- net/mail/index.go
+++ net/mail/index.go
@@ -2,6 +2,16 @@ package mail
 
 import "github.com/xurwxj/viper"
 
+// need following config in config.json:
+// "email": {
+// 	"cloud": "aliyun",
+// 	"account": "xxx@xxx.com",
+// 	"pwd": "xxx",
+// 	"host": "smtpdm.xxx.com:25",
+// 	"service": {
+// 		"panic": "xxx@xurw.com"
+// 	}
+//   },
 func SendMail(to, from, title, body, mailType string) error {
 	cloud := viper.GetString("email.cloud")
 	if cloud == "" {
