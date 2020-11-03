d2691b897f499a2a3e1fa5af9b5cd736868ba0e8
victor <victor@xurw.com>
2020-11-03 11:28:53 +0800

    refactor Remote

--- net/http.go
+++ net/http.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Remote issues a POST to url with header and body resolved from formBody and env, returns the response body.
-func Remote(url, method string, body io.Reader, header map[string]string) ([]byte, error) {
+func Remote(url, method string, body io.Reader, header map[string]string) ([]byte, string, error) {
 	var req *http.Request
 	var res *http.Response
 	var err error
@@ -20,19 +20,19 @@ func Remote(url, method string, body io.Reader, header map[string]string) ([]byt
 	req, err = http.NewRequest(method, url, body)
 
 	if err != nil {
-		return nil, err
+		return nil, "netErr", err
 	}
 	req = setHeader(req, header)
 	res, err = client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, "netErr", err
 	}
 	defer res.Body.Close()
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		return nil, "parseErr", err
 	}
-	return data, nil
+	return data, "", nil
 }
 
 func setHeader(req *http.Request, header map[string]string) *http.Request {
@@ -57,12 +57,5 @@ func PrintLocalDial(network, addr string) (net.Conn, error) {
 		KeepAlive: 15 * time.Minute,
 	}
 
-	conn, err := dial.Dial(network, addr)
-	if err != nil {
-		return conn, err
-	}
-
-	// fmt.Println("PrintLocalDial connect done, use", conn.LocalAddr().String())
-
-	return conn, err
+	return dial.Dial(network, addr)
 }
